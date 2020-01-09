73e93908d83602c24d677ea580fb15c805e76963
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2020-01-09 06:54:55 +0100

    Update analyse_rnaseq.R

--- analyse_rnaseq.R
+++ analyse_rnaseq.R
@@ -6,7 +6,7 @@ function ()
 
 	mtab4818=function(){
 		data=read.csv("E-MTAB-4818-query-results_mod.fpkms.tsv",sep="\t",header=T)
-		ix=grep("Solyc06g005060|Solyc01g094190|Solyc01g101060|Solyc09g008280|Solyc10g083970",data[,1])
+		ix=grep("Solyc01g094190|Solyc01g101060|Solyc09g008280|Solyc10g083970|Solyc12g099000",data[,1])
 		data_f=data[ix,]
 		rownames(data_f)=data_f[,1]
 		data_f=data_f[,3:dim(data_f)[2]]
@@ -17,7 +17,10 @@ function ()
 			data_f[,x]=(data_f[,x]-my_mean)/my_sd
 		}
 		print(data_f)
+		par(mfrow=c(2,2))
 		pheatmap(data_f,display_numbers=TRUE)
+		pheatmap(cor(data_f),display_numbers=TRUE)
+		pheatmap(cor(t(data_f)),display_numbers=TRUE)
 	}
 
 	mtab4818()
