23d78a627a320a85a5e71cb83c41da74a14f44f4
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2020-03-07 19:52:00 +0100

    Update analyse_rnaseq.R

--- analyse_rnaseq.R
+++ analyse_rnaseq.R
@@ -1,9 +1,11 @@
-function (sel_study) 
+library(pheatmap)
+options(stringsAsFactors=FALSE)
+
+analyse_rnaseq=function (sel_study) 
 {
-	library(pheatmap)
-
-	options(stringsAsFactors=FALSE)
-
+	#
+	# normalization of the data
+	#
 	do_norm=function(data_f){
 		for(x in 1:dim(data_f)[2]){
 			my_mean=mean(data_f[,x])
@@ -13,6 +15,9 @@ function (sel_study)
 		return(data_f)
 	}
 
+	#
+	#	tomato fruit at four developmental stages
+	#
 	mtab4818=function(){
 		data=read.csv("E-MTAB-4818-query-results_mod.fpkms.tsv",sep="\t",header=T)
 		ix=grep("Solyc01g094190|Solyc01g101060|Solyc09g008280|Solyc10g083970|Solyc12g099000",data[,1])
@@ -21,12 +26,14 @@ function (sel_study)
 		data_f=data_f[,3:dim(data_f)[2]]
 		data_f=na.omit(data_f)
 		data_f=do_norm(data_f)
-		pheatmap(data_f,display_numbers=TRUE)
-		pheatmap(cor(data_f),display_numbers=TRUE)
-		pheatmap(cor(t(data_f)),display_numbers=TRUE)
+		print("tomato fruit at four developmental stages")
+		print(colnames(data))
 		return(data_f)
 	}
 
+	#
+	#	root transcriptome in chilling-sensitive tomato (S. lycopersium) c.t. more cold-tolerant (S.moneymaker)
+	#
 	mtab4855=function(){
 		data=read.csv("E-MTAB-4855.txt",dec=",",sep="\t",header=T)
 		print(head(data))
@@ -36,12 +43,14 @@ function (sel_study)
 		data_f=data_f[,2:dim(data_f)[2]]
 		data_f=na.omit(data_f)
 		data_f=do_norm(data_f)
-		pheatmap(data_f,display_numbers=TRUE)
-		pheatmap(cor(data_f),display_numbers=TRUE)
-		pheatmap(cor(t(data_f)),display_numbers=TRUE)
+		print("root transcriptome in chilling-sensitive tomato (S. lycopersium) c.t. more cold-tolerant (S.moneymaker)")
+		print(colnames(data))
 		return(data_f)
 	}
 
+	#
+	#	role of red light in resistance to P. syringae
+	#
 	s12864=function(){
 		data=read.csv("s12864.txt",sep="\t",dec=",",header=T)
 		ix=grep("Solyc01g094190|Solyc01g101060|Solyc09g008280|Solyc10g083970|Solyc12g099000",data[,1])
@@ -50,29 +59,22 @@ function (sel_study)
 		data_f=data_f[,3:dim(data_f)[2]]
 		data_f=na.omit(data_f)
 		data_f=do_norm(data_f)
-		print(data_f)
-		pheatmap(data_f,display_numbers=TRUE)
-		pheatmap(cor(data_f),display_numbers=TRUE)
-		pheatmap(cor(t(data_f)),display_numbers=TRUE)
+		print("role of red light in resistance to P. syringae")
+		print(colnames(data))
 		return(data_f)
 	}
 
 
-
+	#
+	#	low temperature that differ in freezing S. lycopersium / S. habrochaites
+	#
 	s12870=function(sel_picture){
 		data=read.csv("study_1.txt",sep="\t",dec=",",row.names=1)
 		ix=grep("Solyc01g094190|Solyc01g101060|Solyc09g008280|Solyc10g083970|Solyc12g099000",rownames(data))
 		data_f=data[ix,2:7]
 		data_f=do_norm(data_f)
-		if(sel_picture==1){
-			pheatmap(data_f,display_numbers=TRUE)
-		}
-		else if(sel_picture==2){
-			pheatmap(cor(data_f),display_numbers=TRUE)
-		}
-		else if(sel_picture==3){
-			pheatmap(cor(t(data_f)),display_numbers=TRUE)
-		}
+		print("low temperature that differ in freezing S. lycopersium / S. habrochaites")
+		print(colnames(data))
 		return(data_f)
 	}
 	if(sel_study=="mtab4818"){
@@ -83,7 +85,7 @@ function (sel_study)
 		h1=s12864()
 	}else if(sel_study=="mtab4855"){
 		h1=mtab4855()
-	}else if(sel_study=="both"){
+	}else if(sel_study=="all"){
 		h1=mtab4818()
 		h2=s12870(1)
 		h3=s12864()
@@ -113,29 +115,18 @@ function (sel_study)
 			rownames(h4)[x]=rn
 		}
 
-		print(head(h1))
-		print(head(h2))
-		print(head(h3))
-		print(head(h4))
-
-
 		uid=unique(rownames(h1),rownames(h2))
 		uid=unique(uid)
-		print(uid)
 
 		h1=h1[uid,]
 		h2=h2[uid,]
 		h3=h3[uid,]
 
-		print(dim(h1))
-		print(dim(h2))
-
 		df=data.frame(h1)
 		df=cbind(df,h2)
 		df=cbind(df,h3)
 		df=cbind(df,h4)
-		print(df)
 
-		pheatmap(df)
+		pheatmap(df,cluster_rows=FALSE)
 	}
 }
